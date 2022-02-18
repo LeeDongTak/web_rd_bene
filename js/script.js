27b59b19915d21096e876cc54da6c61a5cc9b3ae
LeeDongTak <sunny5796@naver.com>
2022-02-18 21:42:19 +0900

    반응형 완료

--- js/script.js
+++ js/script.js
@@ -70,52 +70,32 @@ $(document).ready(function(){
         $(this).removeClass("active");
     });
 
-
-
-
-
-
-
-
-    // 슬라이드가 넘어가면 번호가 바뀌는 이벤트(클릭 동일)
-    // setInterval(function(){
-    //     var newsNum = $(".news > .slider").slick('slickCurrentSlide');  
-    //     $(".nav_text > .now").text(newsNum+1);
-    // },2000);
-    // // eco-round 슬릭슬라이드
-    // $(".eco_round > .eco_slider").slick({
-    //     autoplay: false,
-    //     arrows: true , 
-    //     dots: false ,
-    //     centerrMode: true,
-    //     pauseOnHover: true ,
-    //     slidesToScroll: 1, 
-    //     slidesToShow: 1,
-    //     infinite:true,
-    //     swipe:  false ,
-    //     speed: 1000 ,
-    //     prevArrow: $(".left_btn"),
-    //     nextArrow: $(".right_btn"), 
-    // });
-    // // eco_round 바튼클릭 시 숫자가 바뀌는 이벤트
-    // $(".nav_btn > .right_btn").click(function(){
-    //     var ecoNum = $(".eco_round > .eco_slider").slick('slickCurrentSlide');  
-    //     $(".nav_btn > .now").text(ecoNum+1);
-    //     $(".eco_content").addClass("active");
-    //     var indexNum = $(this).index();
-    //     $(".eco_content").eq(ecoNum+1).addClass("active");
-    //     $(".eco_content").eq(ecoNum+1).siblings().removeClass("active");
-    // });
-    // $(".nav_btn > .left_btn").click(function(){
-    //     var ecoNum = $(".eco_round > .eco_slider").slick('slickCurrentSlide');  
-    //     $(".nav_btn > .now").text(ecoNum+1);
-    //     $(".eco_content").addClass("active");
-    //     var indexNum = $(this).index();
-    //     $(".eco_content").eq(ecoNum+1).addClass("active");
-    //     $(".eco_content").eq(ecoNum+1).siblings().removeClass("active");
-    // });
+    //햄버거 버튼 이벤트
     $(".nobile_wrap > .hamburg_wrap").click(function(){
+        $(".nobile_wrap").toggleClass("active");
         $(".nobile_wrap > .hamburg_wrap").toggleClass("active");
     });  
+    // 반응형 매뉴 이벤트
+    $(".menu_box > li").click(function(){
+        $(this).toggleClass("active");
+        $(this).siblings().removeClass("active");
+    });  
+    // 슬릭슬라이더 (반응형 hello_again)
+    $(".hello_again_wrap > .m_hello_again > .content_box").slick({
+        autoplay: true,
+        autoplaySpeed: 3000 ,
+        arrows: true , 
+        dots: false ,
+        centerrMode: true,
+        pauseOnHover: true ,
+        slidesToScroll: 1, 
+        slidesToShow: 1,
+        infinite:true,
+        swipe:  true ,
+        speed: 1000 ,
+        prevArrow: $(".mo_prev_btn"),
+        nextArrow: $(".mo_next_btn"), 
+        initialSlide:0,
+    });
     AOS.init();
 }); 
